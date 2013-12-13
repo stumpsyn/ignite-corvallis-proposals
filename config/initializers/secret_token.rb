eaa8363734bb778dc144d26818ff46c0c9b1201a
Reid Beels <mail@reidbeels.com>
2013-12-12 16:15:04 -0800

    Generate a new Rails 4.0.2 app to host OCW

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IgnitePortland::Application.config.secret_key_base = '54fcb08a9beafba47d16daccc47840b2a825f0636c979b569b16ed5aa64273adf98f65c3ffda12c4f0cc5e63b14e98edc5c2e3ca357c13df1288085da296a83d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IgnitePortland::Application.config.session_store :cookie_store, key: '_ignite_portland_session'
